a97ea275f3191a2eaf617e5cfd14030acfb1ad7c
Pratik Koli <pratikkoli0403@gmail.com>
2026-01-11 07:30:02 +0000

    adding log parser to picke events

--- core/log_parser.py
+++ core/log_parser.py
@@ -1,1 +1,30 @@
-py
+import re 
+from datetime import datetime
+from core.event import Event
+
+class LogParser:
+    def parse(self, log_line):
+        #default value
+        event_type ="UNKNOWN"
+        source_ip = "UNKNOWN"
+
+
+        # Detect failed login
+        if "Failed password" in log_line:
+            event_type = "FAILED_LOGIN"
+        
+        #detect successful login
+        elif "Accepted password" in log_line:
+            event_type = "SUCCESS_LOGIN"
+            
+        # Extract source IP address using regex
+        ip_match = re.search(r"(\d+\.\d+\.\d+\.\d+)", log_line)
+        if ip_match:
+            source_ip = ip_match.group(1)
+        
+        # Timestamp (simplified for now)
+        timestamp = log_line[:15]
+
+        return Event(timestamp, source_ip, event_type, log_line)
+    
+        
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from core.log_reader import LogReader
+from core.log_parser import LogParser
 
 reader = LogReader("logs/sample_auth.log")
+parser = LogParser()
+
 logs = reader.read_logs()
 
+events = []
 for log in logs:
-    print(log)
+    event = parser.parse(log)
+    events.append(event)
+
+for event in events:
+    print(event)
