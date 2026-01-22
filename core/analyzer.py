27c244a89a7e4a4175039ef578bbbb7bc73cc5f3
Pratik Koli <pratikkoli0403@gmail.com>
2026-01-22 13:59:01 +0000

    analyzer code added with updated main.py

--- core/analyzer.py
+++ core/analyzer.py
@@ -1,2 +1,19 @@
-py
+from collections import defaultdict
 
+class Analyzer:
+    def __init__(self,events):
+        self.events = events
+    
+    def detect_failed_logins(self, threshold=3):
+        failed_attempts = defaultdict(int)
+        suspicious_ips = []
+
+        for event in self.events:
+            if event.event_type == "FAILED_LOGIN":
+                failed_attempts[event.source_ip] +=1
+        
+        for ip, count in failed_attempts.items():
+            if count >= threshold:
+                suspicious_ips.append((ip, count))
+        
+        return suspicious_ips
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from core.log_reader import LogReader
 from core.log_parser import LogParser
+from core.analyzer import Analyzer
 
 reader = LogReader("logs/sample_auth.log")
 parser = LogParser()
 
 logs = reader.read_logs()
+events =[parser.parse(log) for log in logs]
 
-events = []
-for log in logs:
-    event = parser.parse(log)
-    events.append(event)
+analyzer = Analyzer(events)
+results = analyzer.detect_failed_logins()
 
-for event in events:
-    print(event)
+print("Supicious IPs:")
+for ip, count in results:
+    print(f"{ip} -> {count} failed attempts")
